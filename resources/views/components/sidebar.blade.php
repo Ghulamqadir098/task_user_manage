d25ffcdcbd585c4df8e39733036c1bd89f797694
Ghulam Qadir <idontknow131422@gmail.com>
2024-08-19 17:41:15 +0500

    Second commit

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,14 +3,16 @@
         <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
-            <li class="dropdown">
-                <a href="#">Task</a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">Create Task</a></li>
-                    <li><a href="#">My Tasks</a></li>
-                    <li><a href="#">Marketing</a></li>
-                </ul>
-            </li>
+@role('admin')
+<li class="dropdown">
+    <a href="#">Task</a>
+    <ul class="dropdown-menu">
+        <li><a href="#">Create Task</a></li>
+        <li><a href="#">My Tasks</a></li>
+        <li><a href="#">Marketing</a></li>
+    </ul>
+</li>
+@endrole
             <li><a href="#">Contact</a></li>
             <li><a href="{{route('logout')}}">Logout</a></li>
         </ul>
