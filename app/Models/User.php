35de1274633a1b673c0387a6ecb9ea8c454fa384
Ghulam Qadir <idontknow131422@gmail.com>
2024-08-19 18:02:12 +0500

    Third commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,11 +18,19 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
+
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+    
 
     /**
      * The attributes that should be hidden for serialization.
